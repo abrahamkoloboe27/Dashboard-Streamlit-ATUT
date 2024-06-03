e9e0017a4e207d37877167010bffe6fa057c5d50
Abraham KOLOBOE <114146395+abrahamkoloboe27@users.noreply.github.com>
2024-06-03 22:37:31 +0100

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -76,55 +76,54 @@ if file is not None:
           with col_2:
               plot_donut_chart_selected_tutorials(data, st.session_state.selected_tutorials)
 else : 
-    st.markdown("""
-    # Dashboard-Streamlit-ATUT
-
-## Description
-Dashboard-Streamlit-ATUT est une application Streamlit conçue pour ATUT (African Trade Unionist Training) afin de visualiser et analyser diverses métriques de données. Ce projet offre des tableaux de bord interactifs, facilitant une prise de décision éclairée et des insights basés sur les données.
-
-## Fonctionnalités
-- Visualisation de données interactive
-- Interface conviviale
-- Mises à jour des données en temps réel
-- Tableaux de bord personnalisables
-
-## Installation
-1. Clonez le dépôt :
-   ```bash
-   git clone https://github.com/abrahamkoloboe27/Dashboard-Streamlit-ATUT.git
-   ```
-2. Accédez au répertoire du projet :
-   ```bash
-   cd Dashboard-Streamlit-ATUT
-   ```
-3. Installez les dépendances requises :
-   ```bash
-   pip install -r requirements.txt
-   ```
-
-## Utilisation
-1. Lancez l'application Streamlit :
-   ```bash
-   streamlit run Dashboard.py
-   ```
-2. Ouvrez votre navigateur web et allez à `http://localhost:8501` pour accéder au tableau de bord.
-
-## Démo Vidéo
-Pour voir une démonstration de l'application, regardez cette vidéo :
-
-[![Regardez la vidéo démo ](https://img.youtube.com/vi/8l90vuGmUhY/0.jpg)](https://www.youtube.com/watch?v=8l90vuGmUhY&ab_channel=AbrahamKoloboe)
-
-
-Cette vidéo montre comment utiliser les différentes fonctionnalités de l'application, y compris la visualisation des données en temps réel et la personnalisation des tableaux de bord.
-
-## Contribution
-Les contributions sont les bienvenues ! Veuillez forker le dépôt et soumettre une pull request. Si vous avez des idées d'améliorations ou des suggestions, n'hésitez pas à laisser vos commentaires et avis.
-
-## Contact
-Pour toute question ou problème, veuillez contacter Abraham Koloboe à [abklb27@gmail.com](abklb27@gmail.com).
-
-## Licence
-Ce projet est sous licence MIT. Voir le fichier [LICENSE](LICENSE) pour plus de détails.
-```
-
-    """)
+    if st.sidebar.checkbox("Readme", True) :
+        st.markdown("""   
+    ## Description
+    Dashboard-Streamlit-ATUT est une application Streamlit conçue pour ATUT (African Trade Unionist Training) afin de visualiser et analyser diverses métriques de données. Ce projet offre des tableaux de bord interactifs, facilitant une prise de décision éclairée et des insights basés sur les données.
+    
+    ## Fonctionnalités
+    - Visualisation de données interactive
+    - Interface conviviale
+    - Mises à jour des données en temps réel
+    - Tableaux de bord personnalisables
+    
+    ## Installation
+    1. Clonez le dépôt :
+       ```bash
+       git clone https://github.com/abrahamkoloboe27/Dashboard-Streamlit-ATUT.git
+       ```
+    2. Accédez au répertoire du projet :
+       ```bash
+       cd Dashboard-Streamlit-ATUT
+       ```
+    3. Installez les dépendances requises :
+       ```bash
+       pip install -r requirements.txt
+       ```
+    
+    ## Utilisation
+    1. Lancez l'application Streamlit :
+       ```bash
+       streamlit run Dashboard.py
+       ```
+    2. Ouvrez votre navigateur web et allez à `http://localhost:8501` pour accéder au tableau de bord.
+    
+    ## Démo Vidéo
+    Pour voir une démonstration de l'application, regardez cette vidéo :
+    
+    [![Regardez la vidéo démo ](https://img.youtube.com/vi/8l90vuGmUhY/0.jpg)](https://www.youtube.com/watch?v=8l90vuGmUhY&ab_channel=AbrahamKoloboe)
+    
+    
+    Cette vidéo montre comment utiliser les différentes fonctionnalités de l'application, y compris la visualisation des données en temps réel et la personnalisation des tableaux de bord.
+    
+    ## Contribution
+    Les contributions sont les bienvenues ! Veuillez forker le dépôt et soumettre une pull request. Si vous avez des idées d'améliorations ou des suggestions, n'hésitez pas à laisser vos commentaires et avis.
+    
+    ## Contact
+    Pour toute question ou problème, veuillez contacter Abraham Koloboe à [abklb27@gmail.com](abklb27@gmail.com).
+    
+    ## Licence
+    Ce projet est sous licence MIT. Voir le fichier [LICENSE](LICENSE) pour plus de détails.
+    ```
+    
+        """)
