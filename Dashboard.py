1dc77a3d5139d963cde2bf23747fb7a92f7502c9
Abraham KOLOBOE <114146395+abrahamkoloboe27@users.noreply.github.com>
2024-06-03 16:49:31 +0100

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -19,7 +19,7 @@ st.set_page_config(
     page_icon=":📊",
     layout="wide",
 )
-st.title(":blue[Tableau de bord ATUT 2024]",divider='rainbow')
+st.title(":blue[Tableau de bord ATUT 2024]")
 
 # Load data
 with st.expander("Importez les données", False) : 
